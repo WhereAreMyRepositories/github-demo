bf7926d60b8bce35961b0bf10b9c0650243d9fda
Mateusz Buczek <mateuszbuczek.air@gmail.com>
2018-03-12 21:05:54 +0100

    Added new method, kind of reversing previous one

--- c#_projects/ToUpperAfter32/ToUpperAfter32/Program.cs
+++ c#_projects/ToUpperAfter32/ToUpperAfter32/Program.cs
@@ -21,13 +21,34 @@ namespace ToUpperAfter32
             sentence = string.Concat(s);
             sentence = sentence.Replace(' ', (char)7F); 
         }
+
+        static void Re(StringBuilder text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ((text[i] == ' ') && (i != text.Length - 1) && (char.IsLetter(text[i + 1])))
+                {
+                    text[i + 1] = char.ToUpper(text[i + 1]);
+                    text.Remove(i, 1); // or text.Replace(" ", string.Empty);  ?
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            /////////////// Format1 //////////////////////////////////////////////////////////////////////////////////////
             string @string = "ASD 123 fgh jkl";
             Console.WriteLine(@string);
 
             Transform(ref @string);
             Console.WriteLine(@string);
+
+            /////////////// Format2 ////////////////////////////////////////////////////////////////////
+
+            var text = new StringBuilder("I love reading technical docs.");
+            Console.WriteLine(text);
+            Re(text);
+            Console.WriteLine(text);
+
 
             Console.ReadKey();
         }
